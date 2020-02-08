8bcf6742a35a0130a5dc35d7d18de7cd2a314956
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-02-07 21:13:25 -0800

    Updates

---
+++ precache-manifest.b4fd50d1773183e2d4e42893d6d4281a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaedf5c056934ef0cf6d1367d4882f1e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4b58f1d580988f2222c7",
+    "url": "/clicky-game/static/css/main.223a672e.chunk.css"
+  },
+  {
+    "revision": "92e68c3a1cfe74c9d40e",
+    "url": "/clicky-game/static/js/2.a1228723.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.a1228723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b58f1d580988f2222c7",
+    "url": "/clicky-game/static/js/main.aceb2c55.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
